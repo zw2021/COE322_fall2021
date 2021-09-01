db8e00b24e6c45461102ecf78b0948db35910fa6
zw2021 <zoelle.wong@utexas.edu>
2021-09-01 10:46:06 -0500

    fixed algorithem; adding for loop

--- homeworkOne/exercise6.12.cc
+++ homeworkOne/exercise6.12.cc
@@ -3,27 +3,40 @@
 using namespace std;
 
 int main(){
-	int num;
-	int dummy;
+	int dummy, counter, num;
+    int num_iterations[1000];
 
-	cout << "Enter a number: ";
+    /*cout << "Enter a number: ";
 	cin >> num;
-	cout << "You entered: " << num << ",Commencing Collatz conjecture" << endl;
+	cout << "You entered: " << num << ",Commencing Collatz conjecture" << endl;*/
+	
+	dummy = 10; // arbitrary number for placeholder
+    counter = 0;
+    /*for(int ii=2; ii<=1000; ii++)
+    {
+        cout << ii << endl;
+        counter = 0; // reset counter after every iteration
+        num = ii;
 
-	if (num == 1){
-		cout << "Answer is 1" << endl;
-	}
-	
-	dummy = 10; // random number
-	while (num != 1){				
-		if (num % 2) == 0{
-			dummy = num/2;				
-		}else if (num % 2) != 0{
-			dummy = 3*num + 1;
-		}
-	
-		num = dummy;
-	}
-	//cout << "Hello World!" << endl;
+        while (num != 1){
+            if ((num % 2) == 0){
+                dummy = num/2;
+                // cout << "this is even" << endl;  // sanity checks
+            }else if ((num % 2) != 0){
+                dummy = 3*num + 1;
+                // cout << "this is odd" << endl; // sanity checks
+            }
+            num = dummy;
+            ++counter;
+        }
+
+        num_iterations[ii] = counter;
+       // cout << "Number: " << ii << endl;
+       // cout << "Number of Iterations: " << num_iterations[ii] << endl;
+    }*/
+
+    //cout << "Congratulations! Your Number Became " << num << endl;
+    //cout << "Number of Iterations: " << counter << endl;
+	//cout << "End of Computation" << endl;
 	return 0;
 }
