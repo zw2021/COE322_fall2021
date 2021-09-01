8a21e834a256d31c8ae9615a989fe92f504fa87c
zw2021 <zoelle.wong@utexas.edu>
2021-09-01 17:04:17 -0500

    finished for loop; documented code's functions

--- homeworkOne/exercise6.12.cc
+++ homeworkOne/exercise6.12.cc
@@ -1,42 +1,64 @@
 #include<iostream>
-
 using namespace std;
 
-int main(){
-	int dummy, counter, num;
-    int num_iterations[1000];
+//## <SUMMARY> #############################################################################
+/*This code uses the Collatz Conjecture, which states that no matter the starting guess u_1,
+the sequence as n approaches u_n will always terminate at 1. This is true when:
 
-    /*cout << "Enter a number: ";
-	cin >> num;
-	cout << "You entered: " << num << ",Commencing Collatz conjecture" << endl;*/
+    u_n+1 = u_n/2 when u_n is even
+    u_n+1 = 3*u_n+1 when u_n is odd
+
+This code tries all starting values u_1 = 1,....,1000 to the values that leads to the
+longest sequence and prints the previous maximum everytime a sequence is longer than
+a previous starting number */
+//##########################################################################################
+int main(){
+	int dummy, counter, num, max;
+    int num_iterations[9999]; // will need to make this a dynamic array later; hard coded bc of hw
 	
 	dummy = 10; // arbitrary number for placeholder
     counter = 0;
-    /*for(int ii=2; ii<=1000; ii++)
+
+
+    for(int ii=1; ii<=1000; ii++)
     {
-        cout << ii << endl;
         counter = 0; // reset counter after every iteration
         num = ii;
 
+        // Take care of the special case when ii = 1
+        if (ii == 1){
+            dummy = 3*num + 1; // we can do this bc we already know 1 is odd
+            while(dummy != 1){
+                if ((dummy % 2) == 0){
+                    dummy = dummy/2;
+                }else if ((num % 2) != 0){
+                    dummy = 3*dummy + 1;
+                }
+                ++counter;
+            }
+            num_iterations[ii] = counter;
+        }
+
+        // for all other numbers not equal to 1
         while (num != 1){
             if ((num % 2) == 0){
                 dummy = num/2;
-                // cout << "this is even" << endl;  // sanity checks
             }else if ((num % 2) != 0){
                 dummy = 3*num + 1;
-                // cout << "this is odd" << endl; // sanity checks
             }
             num = dummy;
             ++counter;
         }
 
         num_iterations[ii] = counter;
-       // cout << "Number: " << ii << endl;
-       // cout << "Number of Iterations: " << num_iterations[ii] << endl;
-    }*/
 
-    //cout << "Congratulations! Your Number Became " << num << endl;
-    //cout << "Number of Iterations: " << counter << endl;
-	//cout << "End of Computation" << endl;
+        for (int jj = 1; jj <= 1000; jj++)  // Find Running Maximum Value in Array
+            if (num_iterations[jj] > max){
+                max = num_iterations[jj];
+                cout << "Current Maximum Starting Number: " << jj << endl;
+            }
+    }
+
+	cout << "End of Computation" << endl;
 	return 0;
 }
