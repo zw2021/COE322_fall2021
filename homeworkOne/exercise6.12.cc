b7ec80512514be802bd6599b382a8540eb63b296
zwong <zwong@isp02.tacc.utexas.edu>
2021-09-01 07:58:29 -0500

    pushing Collatz algorithm code

--- homeworkOne/exercise6.12.cc
+++ homeworkOne/exercise6.12.cc
@@ -3,6 +3,27 @@
 using namespace std;
 
 int main(){
-	cout << "Hello World!" << endl;
+	int num;
+	int dummy;
+
+	cout << "Enter a number: ";
+	cin >> num;
+	cout << "You entered: " << num << ",Commencing Collatz conjecture" << endl;
+
+	if (num == 1){
+		cout << "Answer is 1" << endl;
+	}
+	
+	dummy = 10; // random number
+	while (num != 1){				
+		if (num % 2) == 0{
+			dummy = num/2;				
+		}else if (num % 2) != 0{
+			dummy = 3*num + 1;
+		}
+	
+		num = dummy;
+	}
+	//cout << "Hello World!" << endl;
 	return 0;
 }
