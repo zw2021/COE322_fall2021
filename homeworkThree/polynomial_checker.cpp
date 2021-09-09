5482223fb946b16c81bcecd9d461ae1a25c5dd53
zw2021 <zoelle.wong@utexas.edu>
2021-09-09 18:06:08 -0500

    first commit

---
+++ homeworkThree/polynomial_checker.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// a polynomial is NOT proper if:
+// the power of the variable is raised to a negative value e.g x^-2
+// the power of the variable is raised to a fraction e.g x^1/2
+
+int proper_polynomial(){
+    // check if the power of the variable is a non negtative value
+    // power of the variable is raised to a fraction
+
+    return 0;
+}
+int main() {
+    return 0;
+}
