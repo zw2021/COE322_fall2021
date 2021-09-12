91c48ae4be2a6ddf4f753c9452d0b35b1bd0ba1b
ndelurgio <delurgion@gmail.com>
2021-09-12 10:43:26 -0500

    changes

--- homeworkThree/main.cpp
+++ homeworkThree/main.cpp
@@ -1,14 +1,13 @@
 //EX 10
 #include <iostream>
 #include <vector>
-#include "evaluate_at.cpp"
+#include "evaluate_at.cpp"  
+#include "find_zero.cpp"
 using namespace std;
 
 int main() {
-    vector<double> coefficients; //Add from other problems
-    double left;
-    double right;
-    double zero = find_zero(coefficients,left,right); //Pavan will add find_zero function
+    vector<double> coefficients = {}; //Add from other problems
+    double zero = find_zero(coefficients); //Pavan will add find_zero function
 
     cout << "Found Root: " << zero << endl;
     cout << "With Value: " << evaluate_at(coefficients, zero);
