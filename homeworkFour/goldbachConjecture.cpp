b5db2b2792d78e767c07682c54809a6ce3319b01
zw2021 <zoelle.wong@utexas.edu>
2021-09-25 23:10:37 -0500

    fixed bug in summation algorithm

--- homeworkFour/goldbachConjecture.cpp
+++ homeworkFour/goldbachConjecture.cpp
@@ -15,58 +15,80 @@ public:
         how_many_primes_found = 0;
         last_number_tested = 1;
     }
-int nextprime() {
-    bool foundprime = false;
-    while(!foundprime) {
-        last_number_tested++;
-        foundprime = true;
-        for (int i = 2; i < last_number_tested; i++) {
-        if (last_number_tested % i == 0) {
-        foundprime = false;
-        break; }
+    int nextprime() {
+        bool foundprime = false;
+        while(!foundprime) {
+            last_number_tested++;
+            foundprime = true;
+            for (int i = 2; i < last_number_tested; i++) {
+            if (last_number_tested % i == 0) {
+            foundprime = false;
+            break; }
+            }
         }
+        how_many_primes_found++;
+        return last_number_tested;
     }
-    how_many_primes_found++;
-    return last_number_tested;
-}
-int number_of_primes_found() {
-    return how_many_primes_found;
-}
+    int number_of_primes_found() {
+        return how_many_primes_found;
+    }
 
-int primes_found(){
-    return last_number_tested;
-};
+    int primes_found(){
+        return last_number_tested;
+    };
     int clear_values(){
         how_many_primes_found = 0;
         last_number_tested = 1;
         return how_many_primes_found, last_number_tested;
     }
+
+    // recursive algorithm that sums each element in prime vector to test conjecture
+    int checkPrime(vector<int> vec, int idx, int jdx, int pivot, int even_num){
+    // counters int idx = 0; int jdx = 1; and when i switches - "pivot"
+        if (jdx > vec.size()-1){
+            pivot ++;
+            idx = pivot;
+            jdx = pivot + 1;
+        }
+            if(vec[idx] + vec[jdx] == even_num){ // base case 2 numbers sum to even number
+            cout << "These primes sum to the selected even number: " << vec[idx] << " and " << vec[jdx] << endl;
+            return vec[idx], vec[jdx];
+        }else if (vec[idx] + vec[idx] == even_num){ // a prime plus itself sums to even number
+            cout << "This prime sums to the selected even number: " << vec[idx] << endl;
+        }else if (vec[idx] + vec[jdx] > even_num  || vec[idx] + vec[jdx] < even_num){ // doesn't sum to even number
+            jdx++;
+            return checkPrime(vec, idx, jdx, pivot, even_num);
+        }else if (vec[pivot] == vec.back()){ // reach end of vector and never reaches sum to even number
+            cout << "None of the primes in the given vector will sum to the selected even number" << endl;
+            return -1; // arbitrary number to show error
+    }
+    return  0;
+    }
+
+    // where the magic happens; finds all the prime numbers for a given vector of even numbers
+    int goldbackConjecture(vector<int> even_numbers){
+        for(int jj = 0; jj < even_numbers.size(); jj++){
+            int prime = 0;
+            vector<int> prime_vec;
+            while(prime < even_numbers[jj]){
+                    prime = nextprime();
+                    cout << "this is prime: " << prime << endl;
+                    if (prime <= even_numbers[jj]){
+                        prime_vec.push_back(prime);
+                    }
+                }
+                clear_values();
+                int idx = 0; int jdx = 1; int pivot = 0;
+                checkPrime(prime_vec, idx, jdx, pivot, even_numbers[jj]);
+         }
+         return 0;
+        }
 };
 
 int main() {
 
-    // run next prime until last number tested = input number
-    vector<int> even_numbers{12};
+    vector<int> even_numbers{8,4,6, 10, 12};// just can't use 2
     primegenerator sequence;
-
-    for(int jj = 0; jj <= even_numbers.size(); jj++){//even_number = 4;
-        int prime = 0;
-        vector<int> prime_vec;
-        while(prime < even_numbers[jj]){
-                prime = sequence.nextprime();
-                cout << "this is prime: " << prime << endl;
-                if (prime < even_numbers[jj]){
-                    prime_vec.push_back(prime);
-                }
-            }
-        sequence.clear_values();
-        for(int ii = 0; ii < prime_vec.size(); ii++){
-            int num = prime_vec.back() + prime_vec[ii];
-            cout << "this is a sum " << num << endl;
-            if(num == even_numbers[jj]){
-                    cout << prime_vec[ii] << " and " << prime_vec.back() << endl;
-                }
-            }
-        }
+    sequence.goldbackConjecture(even_numbers);
             return  0;
 }
--- homeworkFour/sum.cpp
+++ homeworkFour/sum.cpp
@@ -6,25 +6,31 @@
 #include <vector>
 using namespace std;
 
-int checkPrime(vector<int> vec, int idx = 0, int jdx = 1, int pivot = 0){
-    // inputs int idx = 0; int jdx = 1;
-    if (jdx > vec.size()){
-        //need to reset the index to the next element
+int checkPrime(vector<int> vec, int idx, int jdx, int pivot, int even_num){
+    // counters int idx = 0; int jdx = 1; and when i switches - "pivot"
+    if (jdx > vec.size()-1){
         pivot ++;
         idx = pivot;
         jdx = pivot + 1;
     }
-    if((vec[idx] + vec[jdx] == 12) || vec[idx-1] == vec.back()){
+    if(vec[idx] + vec[jdx] == even_num){ // base case 2 numbers sum to even number
+        cout << "These primes sum to the selected even number: " << vec[idx] << " and " << vec[jdx] << endl;
         return vec[idx], vec[jdx];
-    }else if ((vec[idx] + vec[jdx] > 12 ) || (vec[idx] + vec[jdx+1] < 12)){
+    }else if (vec[idx] + vec[idx] == even_num){ // a prime plus itself sums to even number
+        cout << "This prime sums to the selected even number: " << vec[idx] << endl;
+    }else if (vec[idx] + vec[jdx] > even_num  || vec[idx] + vec[jdx] < even_num){ // doesn't sum to even number
         jdx++;
-        checkPrime(vec, idx, jdx, pivot);
+        return checkPrime(vec, idx, jdx, pivot, even_num);
+    }else if (vec[pivot] == vec.back()){ // reach end of vector and never reaches sum to even number
+        cout << "None of the primes in the given vector will sum to the selected even number" << endl;
+        return -1; // arbitrary number to show error
     }
+    return  0;
 }
 int main(){
-    vector<int> myVec{2,3,5,7,11};
-    int a = checkPrime(myVec);
-    cout << a << endl;
-    cout << myVec[5] << endl;
+    vector<int> myVec{2,3,5};
+    int idx = 0; int jdx = 1; int pivot = 0; int even_num = 6;
+    checkPrime(myVec, idx, jdx, pivot, even_num);
     return 0;
 }
+
