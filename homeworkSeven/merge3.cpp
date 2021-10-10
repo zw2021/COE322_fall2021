c7db7e2dd60c53382d7bed776e557529b533f0cb
pavan99 <pavan99@isp02.tacc.utexas.edu>
2021-10-10 01:56:40 -0500

    merged code, not sure if implementation is correct

---
+++ homeworkSeven/merge3.cpp
@@ -0,0 +1,51 @@
+//
+// Created by huang on 10/8/2021.
+//
+
+
+/****************************************************************
+ ****
+ **** This file belongs with the course
+ **** Introduction to Scientific Programming in C++/Fortran2003
+ **** copyright 2016-2021 Victor Eijkhout eijkhout@tacc.utexas.edu
+ ****
+ **** newton1.cxx : newton scalar example
+ ****
+ ****************************************************************/
+/* Helpful Sources for learning about NR method for square root estimation:
+ * http://www.sosmath.com/calculus/diff/der07/der07.html
+ * https://www.quora.com/What-is-the-Newton-Rapson-method-for-finding-a-square-root-of-an-integer-number-How-does-one-use-it
+ *
+ * */
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+#include <cmath>
+
+#include <functional>
+using std::function;
+
+// finds root with one lambda argument
+double newton_root
+(function <double(double)> f){
+double x =1;
+double h = .01;
+while ( true ) {
+auto fx = f(x);
+auto fxh = f(x+h);
+double f_prime = (fxh-fx)/h;
+if (std::abs(fx)<1.e-10 ) break;
+x = x - fx/f_prime;
+}
+cout << "The root of this number is " << x << "\n";
+return x;
+}
+
+int main() {
+    double n;
+    cin >> n;
+    auto f = [n] (double x) -> double{return x*x - n; };
+    newton_root(f);
+return 0;
+}
