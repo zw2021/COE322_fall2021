5e2ad880cc63da31f05583d569e886310b7a9d71
zw2021 <zoelle.wong@utexas.edu>
2021-10-26 18:07:19 -0500

    more info

--- homeworkNine/sort2.cpp
+++ homeworkNine/sort2.cpp
@@ -37,12 +37,32 @@ public:
       if(i_am_first){ // optional argument
             cout << "<< " ;
       }
-      cout << datavalue << ", ";
+      cout << datavalue;
       if (has_next()){
+          cout << ",";
           tail_ptr -> print(false);
-          cout << ">>\n" ;
+      }else{
+          cout << ">>\n";
+      }
       };
 
+  int Sort(shared_ptr<Node> newtail){
+      Node* cur; // current node
+      Node* tail; // tail node
+
+    if (tail_ptr == nullptr){
+        // base case; first node is larger
+          set_tail(newtail);
+   }
+
+   else if(tail_ptr >= cur) & (tail_ptr <= tail){
+       // case when node is in between current and tail
+        tail_ptr -> Sort(newtail);
+   }
+   else{
+       // case when node is greater than the tail
+       tail_ptr -> append(newtail);
+   };
   }
 
   int append(shared_ptr<Node> newtail){
@@ -53,98 +73,19 @@ public:
           tail_ptr -> append(newtail);
        }
   }
+  // head of list is always 0. numbers you're inserting is 0
+  // numbers you're inserting is more than the current.
+  // always in the case
+  // case 1; NO TAIL - append
+  // case 2: between current and next insert
+  // case 3: more than next then insert tail
 
-    /* Given a reference (pointer to pointer)
-to the head of a list and an int, inserts
-a new node on the front of the list. */
-void push(Node** head_ref, int new_data)
-{
-    /* 1. allocate node */
-    Node* new_node = new Node();
 
-    /* 2. put in the data */
-    new_node->data = new_data;
-
-    /* 3. Make next of new node as head */
-    new_node->next = (*head_ref);
-
-    /* 4. move the head to point to the new node */
-    (*head_ref) = new_node;
-}
-
-/* Given a node prev_node, insert a new node after the given
-prev_node */
-void insertAfter(Node* prev_node, int new_data)
-{
-    /*1. check if the given prev_node is NULL */
-    if (prev_node == NULL)
-    {
-        cout<<"The given previous node cannot be NULL";
-        return;
-    }
-
-    /* 2. allocate new node */
-    Node* new_node = new Node();
-
-    /* 3. put in the data */
-    new_node->data = new_data;
-
-    /* 4. Make next of new node as next of prev_node */
-    new_node->next = prev_node->next;
-
-    /* 5. move the next of prev_node as new_node */
-    prev_node->next = new_node;
-}
-
-/* Given a reference (pointer to pointer) to the head
-of a list and an int, appends a new node at the end */
-void append(int new_data)
-{
-    Node** head_ref = NULL;
-    /* 1. allocate node */
-    Node* new_node = new Node();
-
-    Node *last = *head_ref; /* used in step 5*/
-
-    /* 2. put in the data */
-    new_node->data = new_data;
-
-    /* 3. This new node is going to be
-    the last node, so make next of
-    it as NULL*/
-    new_node->next = NULL;
-
-    /* 4. If the Linked List is empty,
-    then make the new node as head */
-    if (*head_ref == NULL)
-    {
-        *head_ref = new_node;
-        return;
-    }
-
-    /* 5. Else traverse till the last node */
-    while (last->next != NULL)
-    {
-        last = last->next;
-    }
-
-    /* 6. Change the next of last node */
-    last->next = new_node;
-    return;
-}
-
-// This function prints contents of
-// linked list starting from head
-void printList(Node *node)
-{
-    while (node != NULL)
-    {
-        cout<<" "<<node->data;
-        node = node->next;
-    }
-}
+// value of the current one and value of the tail pointer
+// base case, no tail just insert node
+// current is a tail, look at info of current, info of tail does it go in between?
+// current and tail info, more than just insert node after tail  - recursive step
 };
-
 
 int main() {
   auto
@@ -155,9 +96,8 @@ int main() {
   first->append(second);
   first -> append(third);
   cout << "List length: "
-       << first->list_length() << endl; 
+       << first->list_length() << endl;
   first->print();
-
   return 0;
 }
 
