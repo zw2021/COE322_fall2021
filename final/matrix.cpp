87c660242ff2f4aa806ba2b4f0f37ebd1f5a0c98
ndelurgio <delurgion@gmail.com>
2021-11-21 23:13:11 -0600

    add complex matrix framework

--- final/matrix.cpp
+++ final/matrix.cpp
@@ -1,74 +1,27 @@
 #include <iostream>
 #include <vector>
+#include <array>
+//#include <..\GSL-main\include\gsl\span>
+#include <span>
+
 
 using namespace std;
+using gsl::span;
 
-class matrix {
+class Matrix {
+
     private:
-    vector<vector<double>> A;
+
+    int m,m,lda;
+    span<double> data;
+
     public:
-     // left public for ease of computations
 
-    //CONSTRUCTOR
-    matrix(vector<vector<double>> matrix) {
-        A = matrix;
-    }
-    matrix(int rows,int cols,double num) { //initializes matrix to num with m rows and n cols
-        vector<double> nums;
-        for(int i = 0; i < cols; i++) {
-            nums.push_back(num);
-        }
-        for(int i = 0; i < rows; i++) {
-            A.push_back(nums);
-        }
-    }
+    Matrix(int m, int lda, int n, double *data) {
 
-    int nrows() {
-        return A.size();
     }
+}
 
-    int ncols() {
-        return A[0].size();
-    }
-
-    double getVal(int row,int col) {
-        return A[row][col];
-    }
-
-    void setVal(int row, int col, double num) {
-        A[row][col] = num;
-    }
-
-    void print() {
-        for(int i = 0; i < nrows(); i++) {
-            for(int j = 0; j < ncols(); j++) {
-                cout << getVal(i,j) << " ";
-            }
-            cout << endl;
-        }
-        cout << endl;
-    }
-
-    matrix simple_product(matrix B) { // Simple, computationally expensive way to multiple matrices. A.simple_product(B) = A*B. Solves Exercise 60.1.
-        matrix C(nrows(), B.ncols(), 0); //Allocates size of solution matrix C
-        for(int i = 0; i < nrows(); i++) {
-            for(int j = 0; j < B.ncols(); j++) {
-                double sum = 0;
-                for (int k = 0; k < ncols(); k++) {
-                    sum += getVal(i,k) * B.getVal(k,j);
-                }
-                C.setVal(i,j,sum);
-            }
-        }
-        return C;
-    }
-};
-
-int main() {
-    matrix A({{1,2,3},{4,5,6}});
-    A.print();
-    matrix B({{1,2},{3,4},{5,6}});
-    B.print();
-    matrix C = A.simple_product(B);
-    C.print();
+int main {
+    return 0;
 }
