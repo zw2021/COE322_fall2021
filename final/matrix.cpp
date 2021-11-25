926291532bc19c23eedb45f6c69517a38e4c7658
ndelur <ndelur@isp02.tacc.utexas.edu>
2021-11-24 22:05:58 -0600

    progress

--- final/matrix.cpp
+++ final/matrix.cpp
@@ -11,6 +11,8 @@
 using namespace std;
 using gsl::span;
 
+
+
 class Matrix {
 
     private:
@@ -68,6 +70,75 @@ class Matrix {
     Matrix Bot(int i) {
         return Matrix(m-i,this->lda,this->n,this->get_double_data() + i);
     }
+
+    //Multiplication functions
+    void MatMult(Matrix& other, Matrix& out) {
+        auto adata = this->get_double_data();
+        auto bdata = other.get_double_data();
+        auto cdata = out.get_double_data();
+        for(int i = 0; i < this->m; i++) {
+            for(int j = 0; j < other.getcols(); j++) {
+                for (int k = 0; k < this->n; k++) {
+                    #ifdef DEBUG
+                        out.at(i,j) += this->at(i,k) * other.at(k,j);
+                    #else
+                        cdata[INDEX(i,j,out.getlda())] += adata[INDEX(i,k,this->lda)] * bdata[INDEX(k,j,other.getlda())];
+                    #endif
+                }
+            }
+        }
+        return;
+    }
+
+    // void BlockedMatMult(Matrix& other, Matrix& out) {
+    //     auto adata = this->get_double_data();
+    //     auto bdata = other.get_double_data();
+    //     auto cdata = out.get_double_data();
+    //     if(this->m != 2 || this->n != 2 || other.getrows() != 2 || other.getcols() != 2 || out.getrows() != 2 || out.getcols() != 2) {
+    //         cout << "Error: BlockedMatMal matrix not 2x2" << endl;
+    //         throw(1);
+    //     }
+    //     #ifdef DEBUG
+    //         out.at(0,0) = this->at(0,0)*other.at(0,0) + this->at(0,1)*other.at(1,0);
+    //         out.at(0,1) = this->at(0,0)*other.at(0,1) + this->at(0,1)*other.at(1,1);
+    //         out.at(1,0) = this->at(1,0)*other.at(0,0) + this->at(1,1)*other.at(1,0);
+    //         out.at(1,1) = this->at(1,0)*other.at(0,1) + this->at(1,1)*other.at(1,1);
+    //     #else
+    //         cdata[INDEX(0,0,out.getlda())] = adata[INDEX(0,0,this->getlda())]*bdata[INDEX(0,0,other.getlda())] + adata[INDEX(0,1,this->getlda())]*bdata[INDEX(1,0,other.getlda())];
+    //         cdata[INDEX(0,1,out.getlda())] = adata[INDEX(0,0,this->getlda())]*bdata[INDEX(0,1,other.getlda())] + adata[INDEX(0,1,this->getlda())]*bdata[INDEX(1,1,other.getlda())];
+    //         cdata[INDEX(1,0,out.getlda())] = adata[INDEX(1,0,this->getlda())]*bdata[INDEX(0,0,other.getlda())] + adata[INDEX(1,1,this->getlda())]*bdata[INDEX(1,0,other.getlda())];
+    //         cdata[INDEX(1,1,out.getlda())] = adata[INDEX(1,0,this->getlda())]*bdata[INDEX(0,1,other.getlda())] + adata[INDEX(1,1,this->getlda())]*bdata[INDEX(1,1,other.getlda())];
+    //     #endif
+    // }
+    void BlockedMatMult(Matrix& other, Matrix& out) {
+        // Matrix aleft = this->Left(this->getcols()/2);
+        // Matrix aright = this->Right(this->getcols()/2);
+        // Matrix atop = this->Top(this->getrows()/2);
+        // Matrix abot = this->Bot(this->getrows()/2);
+
+        // Matrix bleft = other.Left(this->getcols()/2);
+        // Matrix bright = other.Right(this->getcols()/2);
+        // Matrix btop = other.Top(this->getrows()/2);
+        // Matrix bbot = other.Bot(this->getrows()/2);
+
+        Matrix atl = this->Left(this->getcols()/2).Top(this->getrows()/2);
+        Matrix atr = this->Right(this->getcols()/2).Top(this->getrows()/2);
+        Matrix abl = this->Left(this->getcols()/2).Bot(this->getrows()/2);
+        Matrix abr = this->Right(this->getcols()/2).Bot(this->getrows()/2);
+
+        Matrix btl = other.Left(other.getcols()/2).Top(other.getrows()/2);
+        Matrix btr = other.Right(other.getcols()/2).Top(other.getrows()/2);
+        Matrix bbl = other.Left(other.getcols()/2).Bot(other.getrows()/2);
+        Matrix bbr = other.Right(other.getcols()/2).Bot(other.getrows()/2);
+
+        Matrix otl(atl.getrows(),atl.getrows(),btl.getcols(),vector<double>(atl.getrows()*btl.getcols(),0).data());
+        addMatrices(atl.MatMult(btl,otl),atr.MatMult(bbl,otl),otl);
+        otl.print();
+        
+    }
+    //Ex 60.7
+    void RecursiveMatMult(Matrix& other, Matrix& out) {
+    }
     //for testing purposes
     void print() {
         for(int i = 0; i < m; i++) {
@@ -89,15 +160,16 @@ class Matrix {
 };
 
 //Addition method (ex. 60.4)
-Matrix addMatrices(Matrix A, Matrix B) {
+void addMatrices(Matrix A, Matrix B, Matrix& out) {
     if (A.getrows() != B.getrows() || A.getcols() != B.getcols()) {
         cout << "Error using addMatrices: Matrices do no have the same dimensions" << endl;
         throw(1);
     }
 
-    vector<double> cdata(A.getrows()*B.getrows(),0);
+    //vector<double> cdata(A.getrows()*B.getrows(),0);
     auto adata = A.get_double_data();
     auto bdata = B.get_double_data();
+    auto cdata = out.get_double_data();
     for(int j = 0; j < A.getcols(); j++) {
         for(int i = 0; i < A.getrows(); i++) {
             #ifdef DEBUG
@@ -107,9 +179,8 @@ Matrix addMatrices(Matrix A, Matrix B) {
             #endif
         }
     }
-    Matrix C(A.getrows(),A.getrows(),A.getcols(),cdata.data());
-    C.print();
-    return C;
+    //Matrix C(A.getrows(),A.getrows(),A.getcols(),cdata.data());
+    //out = C;
 }
 
 int main() {
@@ -122,15 +193,60 @@ int main() {
     vector<double> data4 = {2,2,3,4,5,6,7,8,9};
     Matrix m1(3,4,3,data3.data());
     Matrix m2(3,3,3,data4.data());
+    Matrix m3(3,3,3,data4.data());
     m1.print();
-    //m2.print();
-    Matrix m3 = addMatrices(m1,m2);
+    m2.print();
+    addMatrices(m1,m2,m3);
+    m3.print();
 
     vector<double> data5 = {1,2,3,4,5,6,7,8,9,10,10,12,13,14,15,16};
     Matrix m4(3,4,4,data5.data());
-    m4.print();
+    //m4.print();
     Matrix l1 = m4.Right(2);
-    l1.print();
+    //l1.print();
+
+    m1.print();
+    m2.print();
+    Matrix m5(3,3,3,vector<double>(9,0).data());
+    m1.MatMult(m2, m5);
+    m5.print();
+
+    vector<double> data6 = {1,2,3,4,5,6,7,8,9};
+    vector<double> data7 = {2,3,4};
+    Matrix m6(3,3,3,data6.data());
+    Matrix m7(3,3,1,data7.data());
+    Matrix m8(3,3,1,vector<double>(3,0).data());
+    m6.MatMult(m7,m8);
+    m6.print();
+    m7.print();
+    m8.print();
+
+
+
+    // vector<double> data9 = {1,2,3,4};
+    // vector<double> data10 = {1,2,3,4};
+    // Matrix m9(2,2,2,data9.data());
+    // Matrix m10(2,2,2,data10.data());
+    // Matrix m11(2,2,2,vector<double>(4,0).data());
+    // m9.BlockedMatMult(m10,m11);
+    // m9.print();
+    // m10.print();
+    // m11.print();
+
+    vector<double> r4c5 = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+    vector<double> r5c4 = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+    vector<double> r4c4 = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+
+    Matrix m20a(4,4,5,r4c5.data());
+    Matrix m20b(5,5,4,r4c5.data());
+    //Matrix m20c(4,4,4,vector<double>(16,0).data());
+    Matrix m20c(4,4,4,r4c4.data());
+    cout << "M20A" << endl;
+    m20a.print();
+    cout << "M20B" << endl;
+    m20b.print();
+    m20a.BlockedMatMult(m20b,m20c);
+
     //m3.print();
     //m3.at(0,0);
     //m3.printdata();
