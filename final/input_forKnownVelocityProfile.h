3242031ffa414b80867fddd684a85e784fc28c2b
zw2021 <zoelle.wong@utexas.edu>
2021-12-02 20:48:07 -0600

    subroutine that creates input that we will multiply the matrix with

--- final/input_forKnownVelocityProfile.h
+++ final/input_forKnownVelocityProfile.h
@@ -28,8 +28,30 @@ double calculate_ezy(double x, double y, double z, double mu) {
     return (3*y*y)/2;
 }
 
+vector<double> surfaceRoughnes_calculator(double LDA, double y_max, double x, double z, double mu){
+    /* Compute the scaling matrix that accounts for boundary layer (BL) interactions at the wall
+     *  These numbers will be very small since the BL are usually ~4 orders of magnitude smaller
+     *  than the wall. Normally we would have to use numerical or analytical equations (e.g Blasius Equations)
+     *  to get values for the wall. But for our application, we will just fill a matrix with random numbers
+     *  between 0 and 1e-5
+     */
+    vector<double> input;
+    double step = y_max/((LDA*LDA)-1); // denominator should always be total number of elements divided 1 - LDA
+
+    // initialize random number for perturbations in the flow
+    std::random_device rd;
+    std::default_random_engine eng(rd());
+    std::uniform_real_distribution<double> distr(0, 1e-5);
+    for(double y = 0; y < y_max; y += step ){
+            double pert =  distr(eng);
+            input.push_back(pert);
+    };
+    return input;
+};
+
 vector<double> perturbedShear_calculator(double LDA, double y_max, double x, double z, double mu) {
-    /*
+    /* Compute shear forces and other perturbation forces that affect shear
+     * Parameters for testing function
     double LDA = 8; // number of rows or columns in a matrix
     double y_max = 50; // radius of the pipe, [m]
     double x = 100.0;
