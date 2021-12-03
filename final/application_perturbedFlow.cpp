3ee53197bd63a10feb69e0d43cde1fe9b7bb88fd
zw2021 <zoelle.wong@utexas.edu>
2021-12-02 21:03:24 -0600

    testing inputs for recursive multiplication function

--- final/application_perturbedFlow.cpp
+++ final/application_perturbedFlow.cpp
@@ -218,9 +218,9 @@ int main() {
     // Test base multiplication
     cout << "Computing Matrix Product with Base Multiplication Function. Result is: " << endl;
     auto start = high_resolution_clock::now();    // time product function
-        Matrix m1(2,8,2,data1.data());
-        Matrix m2(2,8,2,data2.data());
-        Matrix m3(2,8,2,data1.data());
+        Matrix m1(4,8,4,data1.data());
+        Matrix m2(4,8,4,data2.data());
+        Matrix m3(4,8,4,data1.data());
         m3.MatMult(m1,m2);
         m2.print();
     auto stop = high_resolution_clock::now();
@@ -231,15 +231,15 @@ int main() {
     // Test recursive multiplication
     cout << "Computing Matrix Product with Recursive Multiplication Function. Result is: " << endl;
     auto startRecursive = high_resolution_clock::now();    // time product function
-    Matrix mr1(2,8,2,data1.data());
-    Matrix mr2(2,8,2,data2.data());
-    Matrix mr3(2,8,2,data1.data());
-    m3.RecursiveMatMult(m1,m2);
-    m2.print();
+        Matrix mr1(4,8,4,data1.data());
+        Matrix mr2(4,8,4,data2.data());
+        Matrix mr3(4,8,4,data1.data());
+        mr3.RecursiveMatMult(mr1,mr2);
+        mr2.print();
     auto stopRecursive = high_resolution_clock::now();
     auto durationRecursive = duration_cast<microseconds>(stopRecursive - startRecursive);
     cout << "Time taken by Recursive Multiplication function: "
-    << duration.count() << " microseconds" << endl;
+    << durationRecursive.count() << " microseconds" << endl;
 
     return 0;
 }
